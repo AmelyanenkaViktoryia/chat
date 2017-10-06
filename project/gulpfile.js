aba0204b5894761347d5be978b81f6259f2954c1
Viktoryia Amelyanenka <Viktoryia_Amelyanenka@epam.com>
2017-10-06 16:29:29 +0300

    first commit

---
+++ project/gulpfile.js
@@ -0,0 +1,21 @@
+// Объявляем наши плагины
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
+
+// Создаем sass задание
+
+gulp.task('sass', function () {
+    gulp.src('scss/main.scss')
+        .pipe(plumber())
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', function() {
+    gulp.run( 'sass');
+    gulp.watch('scss/**', function(event) {
+        gulp.run('sass');
+    })
+})
