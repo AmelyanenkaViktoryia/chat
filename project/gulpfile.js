c775f74abfdbc24953d3a62827c65beb761a95eb
Amelyanenka Viktoryia <Amelyanenka_Viktoryia@epam.com>
2018-03-28 17:17:23 +0300

    finished 1,3,7 items. Starts 4 item

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,10 +1,7 @@
-// Объявляем наши плагины
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
-
-// Создаем sass задание
 
 gulp.task('sass', function () {
     gulp.src('scss/main.scss')
